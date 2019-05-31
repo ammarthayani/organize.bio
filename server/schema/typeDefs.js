db6a956db1594be63e214db4e59c421adf9b72d9
Ammar Thayani <ammarthayani@gmail.com>
2019-05-30 23:25:01 -0500

    Added all Queries and Mutation Definitions

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -5,7 +5,7 @@ const typeDefs = gql`
 		id: ID
 		name: String
 		users: [User]
-	},
+	}
 
 	type User {
 		id: ID
@@ -14,42 +14,69 @@ const typeDefs = gql`
 		email: String
 		password: String
 		institution: Institution
-	},
+	}
 
-    type Folder {
-        id: ID
-        name: String
-        creator: User
-        templates: [Template]
-    },
-    type Template {
-        id: ID
-        name: String
-        folder: Folder
-        forms: [Form]
-        questions: [Question]
-    },
+	type Folder {
+		id: ID
+		name: String
+		creator: User
+		templates: [Template]
+	}
 
-    type Form {
-        id: ID
-        template: Template
-        answers: [Answer]
-    },
-    type Question {
-        id: ID
-        name: String
-        template: Template
-        answers:[Answer]
-    }
-    type Answer {
-        id: ID
-        name: String
-        question: Question
-        form: Form
-    }
+	type Template {
+		id: ID
+		name: String
+		folder: Folder
+		forms: [Form]
+		questions: [Question]
+	}
+
+	type Form {
+		id: ID
+		template: Template
+		answers: [Answer]
+	}
+
+	type Question {
+		id: ID
+		name: String
+		template: Template
+		answers: [Answer]
+	}
+
+	type Answer {
+		id: ID
+		name: String
+		question: Question
+		form: Form
+	}
+
 	type Query {
-		users: [User],
-        usersByInstitution(InstitutionId: ID!): [User]
+		users: [User]
+		usersByInstitution(InstitutionId: ID!): [User]
+		institution: Institution
+		institutions: [Institution]
+		templates: [Template]
+		templatesByInstitution(InstitutionId: ID!): [Template]
+		templatesByCreator(CreatorID: ID!): [Template]
+		folders: [Folder]
+		foldersByInsitution(InstitutionId: ID!): [Folder]
+		foldersByCreator(CreatorID: ID!): [Folder]
+		forms(TemplateId: ID!): [Form]
+		questions(TemplateId: ID!): [Question]
+		answers(FormId: ID!): [Answer]
+		me(token: String): [User]
+	}
+
+	type Mutation {
+		addInstitution(name: String!): Institution
+		addUser(name: String, username: String, email: String, password: String, institutionId: ID): User
+		login(username: String, password: String): String
+		addTemplate(name: String, CreatorId: ID, FolderId: ID): Template
+		addFolder(name: String, CreatorId: ID): Folder
+		addForm(name: String, TemplateId: ID): Form
+		addQuestion(name: String, TemplateId: ID): Question
+		addAnswer(name: String, FormId: ID, QuestionId: ID): Answer
 	}
 `;
 
