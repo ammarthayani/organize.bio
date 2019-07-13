d80be060bf23632b5a41548cd6949532a704dc7f
Ammar Thayani <ammarthayani@gmail.com>
2019-07-13 13:21:07 -0500

    mm

---
+++ frontend-react/build/precache-manifest.b0595339f33abaa31de7907677bb4ea8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4176c0d563fb6537bfec15eac67f6a1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69b0c8f250279eecb80d",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1a9ac10ccb1d44002e6d",
+    "url": "/static/js/2.eb69c2df.chunk.js"
+  },
+  {
+    "revision": "69b0c8f250279eecb80d",
+    "url": "/static/js/main.e633cb58.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
